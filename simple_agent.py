de887c008b9654c83d02ccb034ef078d0d72f9ee
Hoofar Pourzand <support@lotustech.io>
2019-03-24 07:11:04 -0400

    simple agent for pysc2 latest api stable

--- simple_agent.py
+++ simple_agent.py
@@ -1,8 +1,112 @@
 from pysc2.agents import base_agent
 from pysc2.lib import actions
+from pysc2.lib import features
+
+import time
+
+
+from pysc2.agents import base_agent
+from pysc2.env import sc2_env
+from pysc2.lib import actions, features, units
+from absl import app
+import random
+
+
+# Functions
+_BUILD_SUPPLYDEPOT = actions.FUNCTIONS.Build_SupplyDepot_screen.id
+_NOOP = actions.FUNCTIONS.no_op.id
+_SELECT_POINT = actions.FUNCTIONS.select_point.id
+_RALLY_UNITS_MINIMAP = actions.FUNCTIONS.Rally_Units_minimap.id
+
+# Features
+_PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
+_UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index
+
+# Unit IDs
+_TERRAN_COMMANDCENTER = 18
+_TERRAN_SCV = 45
+
+# Parameters
+_PLAYER_SELF = 1
+_NOT_QUEUED = [0]
+_QUEUED = [1]
+
 
 class SimpleAgent(base_agent.BaseAgent):
+    def __init__(self):
+        super(SimpleAgent, self).__init__()
+
+        self.attack_coordinates = None
+
+    def unit_type_is_selected(self, obs, unit_type):
+        if (len(obs.observation.single_select) > 0 and
+            obs.observation.single_select[0].unit_type == unit_type):
+            return True
+
+        if (len(obs.observation.multi_select) > 0 and
+            obs.observation.multi_select[0].unit_type == unit_type):
+            return True
+
+        return False
+
+    def get_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.feature_units
+            if unit.unit_type == unit_type]
+
+    def can_do(self, obs, action):
+        return action in obs.observation.available_actions
+
+
     def step(self, obs):
         super(SimpleAgent, self).step(obs)
 
-        return actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
+        time.sleep(0.25)
+
+        if obs.first():
+            player_y, player_x = (obs.observation.feature_minimap.player_relative ==
+                                features.PlayerRelative.SELF).nonzero()
+            xmean = player_x.mean()
+            ymean = player_y.mean()
+
+            if xmean <= 31 and ymean <= 31:
+                self.attack_coordinates = (49, 49)
+            else:
+                self.attack_coordinates = (12, 16)
+        #build units here ...
+        return actions.FUNCTIONS.no_op()
+
+
+
+
+def main(unused_argv):
+  agent = SimpleAgent()
+  try:
+    while True:
+      with sc2_env.SC2Env(
+          map_name="Simple64",
+          players=[sc2_env.Agent(sc2_env.Race.terran),
+                   sc2_env.Bot(sc2_env.Race.random,
+                               sc2_env.Difficulty.very_easy)],
+          agent_interface_format=features.AgentInterfaceFormat(
+              feature_dimensions=features.Dimensions(screen=84, minimap=64),
+              use_feature_units=True),
+          step_mul=16,
+          game_steps_per_episode=0,
+          visualize=True) as env:
+
+        agent.setup(env.observation_spec(), env.action_spec())
+
+        timesteps = env.reset()
+        agent.reset()
+
+        while True:
+          step_actions = [agent.step(timesteps[0])]
+          if timesteps[0].last():
+            break
+          timesteps = env.step(step_actions)
+
+  except KeyboardInterrupt:
+    pass
+
+if __name__ == "__main__":
+    app.run(main)
